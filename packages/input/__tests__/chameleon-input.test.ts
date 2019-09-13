0633f543b1296793d7ebde3f271ddb23efe0f2da
Noel Hwande <noel.hwande@maritz.com>
2019-09-13 10:48:55 -0500

    Adds unit test for chameleon-input

---
+++ packages/input/__tests__/chameleon-input.test.ts
@@ -0,0 +1,12 @@
+import { fixture, html, expect } from "@open-wc/testing";
+import "../src/chameleon-input";
+
+describe("chameleon-input", () => {
+  it("renders", async () => {
+    const el = await fixture(html`
+      <chameleon-input></chameleon-input>
+    `);
+    debugger;
+    expect(Boolean(el.shadowRoot)).to.equal(true);
+  });
+});
